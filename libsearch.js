1f899cd9478cc6bf4b32fc49023c25520c38568b
kbwhizz <kbwhizz1@gmail.com>
2020-06-16 22:04:45 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -33,7 +33,7 @@ console.log("End of js file");
 $(document).ready(function() {
          console.log("inside ready");
     //$('div.availabilityRta').on('load', function() {
-    console.log($('body'));
+    console.log($('div.summary'));
     $('div.shortSummary').on('load', function() {                
              console.log("div loaded");
        if($('div.availabilities').length > 0) {
