5fb939503472cff2b0215818cbfeaf5c4f2379dd
kbwhizz <kbwhizz1@gmail.com>
2020-06-18 11:22:04 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -15,36 +15,6 @@
         document.head.appendChild(link);
 
 
-
-/*
-$('div.availabilityRta a').each(function () {
-   console.log($(this).text() );  
-   var LocArr = $(this).text().split(',');
-         console.log(LocArr);
-   var Loc = LocArr[0];
-   if (Loc == 'CRL') {
-      console.log("CRL found");
- $( "<em>Some text</em></br>" ).insertBefore( $(this) );
-   }
-});
-
-*/
-
-/*
-console.log("End of js file");
-$(document).ready(function() {
-         console.log("inside ready");
-    //$('div.availabilityRta').on('load', function() {
-    console.log($('div.summary'));
-    $('div.shortSummary').on('load', function() {                
-             console.log("div loaded");
-       if($('div.availabilities').length > 0) {
-     console.log("RTA loaded");
-}
-    });
-});
-*/
-
 $(document).ready(function() {
 let rootScope = angular.element('html').scope().$root
   rootScope.$on('apiSuccess', (scope) => setTimeout(AppendToHoldings, 1000))
@@ -52,11 +22,9 @@ let rootScope = angular.element('html').scope().$root
 
  function AppendToHoldings() {
           
-          //$('div.availabilityRta').each(function () {
           $('div.availabilityInfo').each(function () {
-          console.log($(this).text() ); 
-          var LocArr = $(this).text().split(',');
-          console.log(LocArr);
+          //console.log($(this).text() ); 
+          var LocArr = $(this).text().split(',');          
           var Loc = LocArr[0].replace(/[\s\n]+/g,'');
           if (Loc == 'CRL') {
               console.log("CRL found");
