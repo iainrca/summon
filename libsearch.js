d9647fa541b9cabaac8d43d8e62d6a74941a1dff
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-18 16:37:34 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -30,19 +30,19 @@ let rootScope = angular.element('html').scope().$root
               console.log("Special Collections Appointment needed");
               $( "<em>Contact <a href='https://www.rca.ac.uk/more/special-collections/access-location-and-appointments/' target='_blank'>Archives and Special Collections</a> to book an appointment</em></br>" ).insertBefore( $(this) );
           } 
-         elseif (Loc == 'SPECCOLL') {
+         else if (Loc == 'SPECCOLL') {
               $( "<em>Contact <a href='https://www.rca.ac.uk/more/special-collections/access-location-and-appointments/' target='_blank'>Archives and Special Collections</a> to book an appointment</em></br>" ).insertBefore( $(this) );
 
           }
-         elseif (Loc == 'ARC') {
+         else if (Loc == 'ARC') {
               $( "<em>Contact <a href='https://www.rca.ac.uk/more/special-collections/access-location-and-appointments/' target='_blank'>Archives and Special Collections</a> to book an appointment</em></br>" ).insertBefore( $(this) );
 
           }
-         elseif (Loc == 'AB') {
+         else if (Loc == 'AB') {
               $( "<em>Contact <a href='https://www.rca.ac.uk/more/special-collections/access-location-and-appointments/' target='_blank'>Archives and Special Collections</a> to book an appointment</em></br>" ).insertBefore( $(this) );
 
           }
-          elseif (Loc == 'FLTV') {
+          else if (Loc == 'FLTV') {
               $( "<em>Contact Library Desk to request item</em></br>" ).insertBefore( $(this) );
 
           }
