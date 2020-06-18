9293a5ec2c633a7c3a46dde77223ab3828db9934
kbwhizz <kbwhizz1@gmail.com>
2020-06-18 11:09:47 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -54,6 +54,13 @@ let rootScope = angular.element('html').scope().$root
           
           //$('div.availabilityRta').each(function () {
           $('div.availabilityInfo').each(function () {
-      console.log($(this).text() ); 
+          console.log($(this).text() ); 
+          var LocArr = $(this).text().split(',');
+          console.log(LocArr);
+          var Loc = LocArr[0];
+          if (Loc == 'CRL') {
+              console.log("CRL found");
+              $( "<em>Some text</em></br>" ).insertBefore( $(this) );
+          }                           
    });
  }
