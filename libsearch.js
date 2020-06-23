062b3cec71798ace62d4e88203a4777f2bc7d412
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-23 17:27:39 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -90,5 +90,26 @@ angular.module('summonApp')
      }
  }]);
 
-$('.customLinks li:contains(Help)').hide();
-$('.customLinks li:contains(Feedback)').hide();
+/* An attempt to change Single Journals to Publisher Site to make the experience for end user better */
+
+$(document).ready(function() {
+let rootScope = angular.element('html').scope().$root
+  rootScope.$on('apiSuccess', (scope) => setTimeout(AmendToPublisher, 2000))
+});
+
+ function AmendToPublisher() {
+         $('a.results-dbRef').each(function () {
+          console.log($(this).text() ); 
+          var LocArr = $(this).text().split(',');          
+          var Loc = LocArr[0].replace(/[\s\n]+/g,'');
+          console.log(Loc);
+          if (Loc == 'SingleJournals') {
+              console.log("Change to Publishers Site");
+              $(this).text("Publishers Site");
+          } 
+		  });
+ }
+
+
+//$('.customLinks li:contains(Help)').hide();
+//$('.customLinks li:contains(Feedback)').hide();
