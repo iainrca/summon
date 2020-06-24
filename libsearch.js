b8905814c3bfb3ef1b114f5b4430eb7de25f718f
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-24 12:53:08 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -32,7 +32,7 @@ let rootScope = angular.element('html').scope().$root
                    //console.log(Loc);
           if (Loc == 'CRL') {
               console.log("Special Collections Appointment needed");
-              $( "<em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk'>special-collections@rca.ac.uk</a> to arrange</em></br>" ).insertBefore( $(this) );
+              $( "<p id='specialcol'><em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk'>special-collections@rca.ac.uk</a> to arrange</em></p></br>" ).insertBefore( $(this) );
           } 
          else if (Loc == 'SPECCOLL') {
               $( "<em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk'>special-collections@rca.ac.uk</a> to arrange</em></br>" ).insertBefore( $(this) );
