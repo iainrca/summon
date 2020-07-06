528463363b56c2be6b0d2bf17f8d6a0a75f5598b
iainrca <66618182+iainrca@users.noreply.github.com>
2020-07-06 16:31:08 +0100

    Update ejp.js

--- ejp.js
+++ ejp.js
@@ -1,14 +1,13 @@
 /* An attempt to change Single Journals to Publisher Site to make the experience for end user better */
-
- function AmendToPublisher() {
-         $('a.results-dbRef').each(function () {
-          console.log($(this).text() ); 
+$(document).ready(function() {
+	 $('a.results-dbRef').each(function () {
+ 
           var DbArr = $(this).text().split(',');          
           var DbType = DbArr[0].replace(/[\s\n]+/g,'');
-          console.log(DbType);
+
           if (DbType == 'SingleJournals') {
               console.log("Change to Publishers Site");
               $(this).text("Publishers Site");
           } 
 		  });
- }
+		 });
