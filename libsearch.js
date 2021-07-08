0026f6317cf8e1da59fcb99f5a60c22703dddf5a
iainrca <66618182+iainrca@users.noreply.github.com>
2021-07-08 09:25:41 +0100

    Update libsearch.js

--- libsearch.js
+++ libsearch.js
@@ -99,7 +99,7 @@ angular.module('summonApp')
          label: "Inter-library Loans"
      }
        config.data.links.custom7 = {
-         href: "http://library.rca.ac.uk/client/en_GB/summon/search/patronlogin/http:$002f$002flibrary.rca.ac.uk$002fclient$002fen_GB$002fsummon$002fsearch$002faccount$002f1$003f",
+         href: "https://library.rca.ac.uk/client/en_GB/summon/search/patronlogin/http:$002f$002flibrary.rca.ac.uk$002fclient$002fen_GB$002fsummon$002fsearch$002faccount$002f1$003f",
          label: "My Account"
      }        
          
