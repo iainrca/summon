5aa9497bdbdc056f620211ef4f0efe9bc0d905eb
iainrca <66618182+iainrca@users.noreply.github.com>
2021-10-20 10:14:52 +0100

    Update alternative_shelfmap.js

--- alternative_shelfmap.js
+++ alternative_shelfmap.js
@@ -37,7 +37,7 @@ let rootScope = angular.element('html').scope().$root
           //["THESES","STACK","FLTV","OFFSITE","PER_NONC","PER_RES","CAT","IP","JR","ONORD"]
 		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
 		  var validSPLocs=["CRL","SPECCOLL","ARC","AB","OFFSITE"];
-		  var validEQLocs=["FLTV"<"PER_NONC","PER_RES","THESES","STACK","STORE"];
+		  var validEQLocs=["FLTV"<"PER_NONC","PER_RES","THESES","STACK","STORE","JR"];
           var LocArr = $(this).text().split(',');          
           var Loc = LocArr[0].replace(/[\s\n]+/g,'');
                    //console.log(Loc);
