c0e231ded92f7c75a1c3027edd2bc2ff6d5ef72c
iainrca <66618182+iainrca@users.noreply.github.com>
2021-10-27 16:43:20 +0100

    Update summon_shelfmap_loadlinkonce.js

--- summon_shelfmap_loadlinkonce.js
+++ summon_shelfmap_loadlinkonce.js
@@ -18,7 +18,7 @@
 
 $(document).ready(function() {
 let rootScope = angular.element('html').scope().$root
-  rootScope.$on('apiSuccess', (scope) => setTimeout(AppendToHoldings, 3000))
+  rootScope.$on('apiSuccess', (scope) => setInterval(AppendToHoldings, 2000))
 });
 
 
