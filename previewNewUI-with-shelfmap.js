c5bcd184e33cf3f22d1453889aa8940bb9020f2e
Bernard Scaife <kbwhizz1@gmail.com>
2021-11-19 15:07:56 +0000

    Update previewNewUI-with-shelfmap.js
    trying find instead of qselector

--- previewNewUI-with-shelfmap.js
+++ previewNewUI-with-shelfmap.js
@@ -44,7 +44,8 @@ let rootScope = angular.element('html').scope().$root
 	 
           $('div.availabilityRta').each(function () {
           console.log($(this));		  
-          console.log($(this).querySelector('div.ng-scope a.summonBtn').getAttribute('title')); 		  
+          //console.log($(this).querySelector('div.ng-scope a.summonBtn').getAttribute('title')); 		  
+	  console.log($(this).find('div.ng-scope a.summonBtn'));	  
 		  
 		  var bibid = $(this).closest($('div.documentSummary')).attr('id').replace(/FETCH-rca_catalog_u/g,'').replace(/.$/,'');	  	  	  	  
 		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
