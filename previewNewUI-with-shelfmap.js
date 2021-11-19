bb684438b919bdc319cadcfd451daa6017ceca51
Bernard Scaife <kbwhizz1@gmail.com>
2021-11-19 15:51:46 +0000

    Create previewNewUI-with-shelfmap.js
    try append instead of insertafter to place span within the div

--- previewNewUI-with-shelfmap.js
+++ previewNewUI-with-shelfmap.js
@@ -61,12 +61,13 @@ let rootScope = angular.element('html').scope().$root
 		     // skip as link already exists
 		  }
 		  else {
-                     $( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>").insertAfter( $(this).find('div.ng-scope') );		
+                     //$( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>").insertAfter( $(this).find('div.ng-scope') );		
+                     $( $(this).find('div.ng-scope') ).append( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>" );			  
 		  }			  
                }
 			if(validSPLocs.indexOf(Loc) !== -1) {
 		// add link to special collections booking form
-				   $( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" ).after( $(this) );        
+				   $( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" ).After( $(this) );        
 				 // $( "<div id='specialcol'><em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk' target='_blank'>special-collections@rca.ac.uk</a> to arrange</em></div></br>" ).insertAfter( $(this) );  
 			} 
 			if(validEQLocs.indexOf(Loc) !== -1) {
