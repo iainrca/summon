6ba5bbac2222b691072450c35a7959edc4418dfa
iainrca <66618182+iainrca@users.noreply.github.com>
2021-11-19 11:47:00 +0000

    Update previewNewUI-with-shelfmap.js

--- previewNewUI-with-shelfmap.js
+++ previewNewUI-with-shelfmap.js
@@ -43,13 +43,13 @@ let rootScope = angular.element('html').scope().$root
 	  console.log("cleared");	 
 	 
           $('div.availabilityRta').each(function () {
-          console.log($(this).attr(title)() ); 
+          console.log($(this).text() ); 
 		  
 		  var bibid = $(this).closest($('div.documentSummary')).attr('id').replace(/FETCH-rca_catalog_u/g,'').replace(/.$/,'');	  	  	  	  
 		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
 		  var validSPLocs=["CRL","SPECCOLL","ARC","AB","OFFSITE"];
 		  var validEQLocs=["FLTV","PER_NONC","PER_RES","THESES","STACK","STORE","JR","CATALOGING","IP","BIN","BOUT"];
-          var LocArr = $(this).attr(title)().split(',');          
+          var LocArr = $(this).text().split(',');          
           var Loc = LocArr[0].replace(/[\s\n]+/g,'');
                    //console.log(Loc);
 			if(validSMLocs.indexOf(Loc) !== -1) {
