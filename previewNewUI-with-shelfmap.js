f42196712a1dd3d28fd0321030210d9a15054e91
iainrca <66618182+iainrca@users.noreply.github.com>
2021-11-19 12:17:46 +0000

    Update previewNewUI-with-shelfmap.js

--- previewNewUI-with-shelfmap.js
+++ previewNewUI-with-shelfmap.js
@@ -49,7 +49,7 @@ let rootScope = angular.element('html').scope().$root
 		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
 		  var validSPLocs=["CRL","SPECCOLL","ARC","AB","OFFSITE"];
 		  var validEQLocs=["FLTV","PER_NONC","PER_RES","THESES","STACK","STORE","JR","CATALOGING","IP","BIN","BOUT"];
-          var LocArr = $(this)querySelector('.summonBtn')getAttribute('title').split(',');          
+          var LocArr = $(this).querySelector('.summonBtn').getAttribute('title').split(',');          
           var Loc = LocArr[0].replace(/[\s\n]+/g,'');
                    //console.log(Loc);
 			if(validSMLocs.indexOf(Loc) !== -1) {
