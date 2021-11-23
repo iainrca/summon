7329d28f4fe4ccc1ccc8daaf0d70a41ab6984947
iainrca <66618182+iainrca@users.noreply.github.com>
2021-11-23 12:16:12 +0000

    Update summon-with-shelfmap.js

--- summon-with-shelfmap.js
+++ summon-with-shelfmap.js
@@ -33,7 +33,7 @@ let rootScope = angular.element('html').scope().$root
 	 var checkExist = setInterval(function() {
          if ($('div.availabilityInfo').length) {
             console.log("Exists!");
-            clearInterval(checkExist);
+            //clearInterval(checkExist);
          }
          else {
             console.log("not present yet");
@@ -70,9 +70,9 @@ let rootScope = angular.element('html').scope().$root
 		     // skip as link already exists
 		  }
 		  else {	
-				  // $( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" ).insertAfter( $(this) );        
+				   $( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" ).insertAfter( $(this) );        
 				 // $( "<div id='specialcol'><em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk' target='_blank'>special-collections@rca.ac.uk</a> to arrange</em></div></br>" ).insertAfter( $(this) );  
-			  	 $( $(this) ).append( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" );
+			  	 //$( $(this) ).append( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" );
 			} 
 			}
 			if(validEQLocs.indexOf(Loc) !== -1) {
