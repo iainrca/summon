746bd5f7f63e80282944be6396ff85e3c29dcc79
iainrca <66618182+iainrca@users.noreply.github.com>
2021-12-13 15:24:54 +0000

    Update previewNewUI-with-shelfmap.js

--- previewNewUI-with-shelfmap.js
+++ previewNewUI-with-shelfmap.js
@@ -43,57 +43,60 @@ let rootScope = angular.element('html').scope().$root
 	  console.log("cleared");	 
 	 
           $('div.availabilityRta').each(function () {
-          	 //console.log($(this));		  
-         	 //console.log($(this).querySelector('div.ng-scope a.summonBtn').getAttribute('title')); 		            
-	  	 //console.log($(this).find('div.ng-scope a.summonBtn').attr('title'));	  
+          //console.log($(this));		  
+          //console.log($(this).querySelector('div.ng-scope a.summonBtn').getAttribute('title')); 		            
+	  //console.log($(this).find('div.ng-scope a.summonBtn').attr('title'));	  
 		  
 		  var bibid = $(this).closest($('div.documentSummary')).attr('id').replace(/FETCH-rca_catalog_u/g,'').replace(/.$/,'');	  	  	  	  
 		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
 		  var validSPLocs=["CRL","SPECCOLL","ARC","AB","OFFSITE"];
 		  var validEQLocs=["FLTV","PER_NONC","PER_RES","THESES","STACK","STORE","JR","CATALOGING","IP","BIN","BOUT"];
-		  
-	  	  var LocArr = $(this).find('div.ng-scope .availabilityLink').attr('title').split(',');
-          	  var Loc = LocArr[0].replace(/[\s\n]+/g,'');
+          //var LocArr = $(this).find('div.ng-scope .summonBtn').attr('title').split(','); 
+	  var LocArr = $(this).find('div.ng-scope .availabilityLink').attr('title').split(',');
+          var Loc = LocArr[0].replace(/[\s\n]+/g,'');
                    console.log(Loc);
 			if(validSMLocs.indexOf(Loc) !== -1) {
-	     			// get bib id and append shelfmap url/link				
-		  		//console.log($(this).find('div.ng-scope a.summonBtn').siblings('span#shelfmap'));		  				
-		  		//if($(this).siblings('span#shelfmap').length) {
-		  		//if($(this).find('div.ng-scope .summonBtn').siblings('span#shelfmap').length) {
-		  		//if($(this).find('div.ng-scope .availabilityLink').siblings('span#shelfmap').length) {
-		  		if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('span#shelfmap').length) {
-		     		// skip as link already exists
-		  			}
-		 		 else { 
-                     			$( $(this).find('div.ng-scope') ).append( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>" );			  
-		  			}			  
-               		}
+	     // get bib id and append shelfmap url/link				
+		  //console.log($(this).find('div.ng-scope a.summonBtn').siblings('span#shelfmap'));		  				
+		  //if($(this).siblings('span#shelfmap').length) {
+		  //if($(this).find('div.ng-scope .summonBtn').siblings('span#shelfmap').length) {
+		  //if($(this).find('div.ng-scope .availabilityLink').siblings('span#shelfmap').length) {
+		  if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('span#shelfmap').length) {
+		     // skip as link already exists
+		  }
+		  else {
+                     //$( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>").insertAfter( $(this).find('div.ng-scope') );		
+                     $( $(this).find('div.ng-scope') ).append( "<span id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></span></br>" );			  
+		  }			  
+               }
 			if(validSPLocs.indexOf(Loc) !== -1) {
-			// add link to special collections booking form 
-				if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('span#shelfmap').length) {
-		    		 // skip as link already exists
+		// add link to special collections booking form 
+				if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('div#specialcol').length) {
+		     // skip as link already exists
 				}
 				else {
-				   $( "<span id='shelfmap'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></span></br>" ).insertAfter( $(this) );  
+					$( $(this).find('div.ng-scope') ).append( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" );
+				   //$( "<div id='specialcol'><em>Item in Special Collections - access by <a href='https://rca.libguides.com/c.php?g=690477&p=4946847&preview=c35121341496a0c2f17dc536ffd730d5' target='_blank'>appointment</a></em></div></br>" ).insertAfter( $(this) );        
+				 // $( "<div id='specialcol'><em>Access by appointment: please email <a href='mailto:special-collections@rca.ac.uk' target='_blank'>special-collections@rca.ac.uk</a> to arrange</em></div></br>" ).insertAfter( $(this) );  
 				} 
 			}
 			if(validEQLocs.indexOf(Loc) !== -1) {
-			// add link to ask at library desk
-				if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('span#shelfmap').length) {
-		     		// skip as link already exists
+		// add link to ask at library desk
+				if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('div#enqdesk').length) {
+		     // skip as link already exists
 				}
 				else {
-				   $( "<span id='shelfmap'><em>Ask at Library Desk</em></br></span>" ).insertAfter( $(this) );        
-			   	} 
+				   $( $(this).find('div.ng-scope') ).append( "<div id='enqdesk'><em>Ask at Library Desk</em></br></div>" );        
+			   } 
 			}
-           		 else if (Loc == 'PER_CURR') {
-			// add link to point to journals room
-				if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('span#shelfmap').length) {
-		     		// skip as link already exists
+            else if (Loc == 'PER_CURR') {
+		// add link to point to journals room - is this necessary now with shelfmap?
+						if($(this).find('div.ng-scope .summonBtn').parent().parent().siblings('div#specialcol').length) {
+		     // skip as link already exists
 				}
 				else {
-				   $( "<span id='shelfmap'><em>Latest Issue in Journals Room</em></br></span>" ).insertAfter( $(this) );
-	    			} 
+				   $( $(this).find('div.ng-scope') ).append( "<div id='specialcol'><em>Latest Issue in Journals Room</em></br></div>" );
+	    } 
 			}
    });
  }
