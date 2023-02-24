90d701d0724d8604dab20b861c2adc29e11ecc20
iainrca <66618182+iainrca@users.noreply.github.com>
2023-02-24 13:40:20 +0000

    Update summon-preview.js

--- summon-preview.js
+++ summon-preview.js
@@ -34,7 +34,10 @@ $('body').append('<style>.quick-look-button{ display: none !important; }</style>
 
 $(document).ready(function() {
 let rootScope = angular.element('html').scope().$root
-  rootScope.$on('apiSuccess', (scope) => setTimeout(AppendToHoldings, 3000))
+  rootScope.$on('apiSuccess', (scope) => setTimeout(AppendToHoldings, 3000));
+$( "head" ).prepend( "<!-- Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WZ4QLHS');</script><!-- End Google Tag Manager -->");
+$( "body" ).prepend( "<!-- Google Tag Manager (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZ4QLHS"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!-- End Google Tag Manager (noscript) -->");
+
 });
 
 
