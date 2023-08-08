d2bc7e6693e7f13092359c1a8766c63bb6590c99
iainrca <66618182+iainrca@users.noreply.github.com>
2023-08-08 18:03:15 +0100

    Update summon.js

--- summon.js
+++ summon.js
@@ -64,7 +64,7 @@ let rootScope = angular.element('html').scope().$root
 	  //console.log($(this).find('div.ng-scope a.summonBtn').attr('title'));	  
 		  
 		  var bibid = $(this).closest($('div.documentSummary')).attr('id').replace(/FETCH-rca_catalog_u/g,'').replace(/.$/,'');	  	  	  	  
-		  var validSMLocs=["ONSHELF","REFERENCE","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
+		  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
 		  var validSPLocs=["CRL","SPECCOLL","ARC","AB","OFFSITE"];
 		  var validEQLocs=["PER_NONC","PER_RES","THESES","STACK","STORE","JR","CATALOGING","IP","BIN","BOUT","QR","PAMPHLET"];
           //var LocArr = $(this).find('div.ng-scope .summonBtn').attr('title').split(','); 
