1fcd147f9e22f51ef9a53ff96f32c3494ecedad0
Bernard Scaife <kbwhizz1@gmail.com>
2024-04-27 12:30:46 +0100

    taking back to summary tag (removing row)

--- summon-preview.js
+++ summon-preview.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     });
 
    //domObserver.observe(document.body, { childList: true, subtree: true });
-   domObserver.observe(document.querySelectorAll("div.documentSummary > div.summary > div.row"), { childList: true, subtree: true });
+   domObserver.observe(document.querySelectorAll("div.documentSummary > div.summary"), { childList: true, subtree: true });
 
 //let rootScope = angular.element('html').scope().$root
 //  rootScope.$on('apiSuccess', (scope) => setTimeout(AppendToHoldings, 3000))
